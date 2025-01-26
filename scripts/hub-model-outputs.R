ab70da3ba9394b7f5ba8f13a7c3f8f4a66e40a24
AI4Casting Hub Admin <4castinghub@uoguelph.ca>
2025-01-26 12:50:40 -0500

    Update hub-model-outputs.R

--- scripts/hub-model-outputs.R
+++ scripts/hub-model-outputs.R
@@ -26,7 +26,7 @@ process_disease <- function(disease, data) {
     preds <- pivot_quantiles_wider(cdc$predictions, .pred_distn) |>
       select(geo_value, ahead, forecast_date, target_date, disease, `0.025`, `0.1`, `0.25`, `0.5`, `0.75`, `0.9`, `0.975`) #elect(geo_value, ahead, forecast_date, target_date, `0.5`,matches("^0\\.\\d{3}$")) |>      
 
-    print(names(preds)
+    print(names(preds))
     preds <- preds |>
       mutate(disease = paste("pct wk", target_simplified[disease], "lab det"))
     
